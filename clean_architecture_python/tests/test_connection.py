6345a36e4bd5f88eac43b99b44321258788b1563
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-07-10 09:56:39 -0300

    Create engine from DataBase

--- clean_architecture_python/tests/test_connection.py
+++ clean_architecture_python/tests/test_connection.py
@@ -4,7 +4,6 @@ from clean_architecture_python.src.infra.db.settings.connection import DBConnect
 @pytest.mark.skip(reason="Sensive Test")
 def test_create_database_engine():
     db_connection_handle = DBConnectionHandle()
-
     engine = db_connection_handle.get_engine()
 
     assert engine is not None
