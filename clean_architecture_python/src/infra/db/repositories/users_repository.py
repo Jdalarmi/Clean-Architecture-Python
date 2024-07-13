884d63291cbd56369cf2ce7302f211f62af843ec
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-07-13 17:35:34 -0300

    Create engine from DataBase

--- clean_architecture_python/src/infra/db/repositories/users_repository.py
+++ clean_architecture_python/src/infra/db/repositories/users_repository.py
@@ -1,5 +1,6 @@
 from src.infra.db.settings.connection import DBConnectionHandle
 from src.infra.db.entites.users import Users as UsersEntity
+import typing as t
 
 class UsersRepository:
 
@@ -14,6 +15,21 @@ class UsersRepository:
                 )
                 database.session.add(new_registry)
                 database.session.commit()
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
+
+    @classmethod
+    def select_user(cls, first_name: str) -> t.Any:
+        with DBConnectionHandle() as database:
+            try:
+                users = (
+                    database.session
+                        .query(UsersEntity)
+                        .filter(UsersEntity.first_name == first_name)
+                        .all()
+                )
+                return users
             except Exception as exception:
                 database.session.rollback()
                 raise exception
